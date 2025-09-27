972c8ff66cf90d4eaf783ace6da0c9122b0e3654
Lammy <aslam.nikware2006@gmail.com>
2025-09-27 17:08:01 +0700

    update

---
+++ main/program.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.title('Login')
+window.geometry('600x500+500+150')
+window.resizable(False,False)
+tk.Label(window,text ="hello World").pack()
+tk.Label(window,text ="hello World").pack()
+
+window.mainloop()
